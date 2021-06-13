734d7d9ea3da3b5afb3d1672a3f28f7fe9aed76f
Angel <aramos@sps-program.com>
2021-06-13 20:21:36 +0000

    Week 2 Completed

--- portfolio/src/main/webapp/secret.js
+++ portfolio/src/main/webapp/secret.js
@@ -1,8 +1,29 @@
 async function showScramPage()
 {
     const responseServer = await fetch('/scram');
-    const textResponse = await responseServer.text();
+    const textResponse = await responseServer.json();
 
     const scramContainer = document.getElementById('scram-container');
-    scramContainer.innerText = textResponse;
+    scramContainer.innerHTML = '';
+
+    //Random message
+    const x = Math.floor((Math.random() * 3) + 1); 
+
+    if(x == 1)
+    {
+      scramContainer.appendChild(createElement(textResponse.Message1));
+    }else if(x == 2)
+    {
+      scramContainer.appendChild(createElement(textResponse.Message2));
+    }else if(x == 3)
+    {
+      scramContainer.appendChild(createElement(textResponse.Message3));
+    }
+}
+
+function createElement(text) 
+{
+  const pElement = document.createElement('p');
+  pElement.innerText = text;
+  return pElement;
 }
